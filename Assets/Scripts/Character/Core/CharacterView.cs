70d85bf81ef6d2926f49366f6b886db5b64a4dde
KimTaeYun <yuntaeyun123@gmail.com>
2026-09-22 02:07:22 +0900

    캐릭터 모델 생성, 파괴하도록 변경

--- Assets/Scripts/Character/Core/CharacterView.cs
+++ Assets/Scripts/Character/Core/CharacterView.cs
@@ -1,47 +1,30 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterView : MonoBehaviour
 {
+    [Header("Model")]
     [SerializeField] private Transform modelRoot;
-
-    private readonly Dictionary<CharacterData, GameObject> viewCache = new();
 
     private GameObject currentView;
 
     public void ChangeView(CharacterData data)
     {
         if (data == null || data.Prefab == null)
-            return;
-
-        // 이미 현재 캐릭터라면 아무것도 안 함
-        if (viewCache.TryGetValue(data, out GameObject cachedView)
-            && cachedView == currentView)
         {
             return;
         }
 
-        // 현재 보이는 캐릭터 끄기
+        // 기존 캐릭터 View 제거
         if (currentView != null)
         {
-            currentView.SetActive(false);
+            Destroy(currentView);
         }
 
-        // 캐시에 없으면 최초 1회 생성
-        if (!viewCache.TryGetValue(data, out GameObject newView))
-        {
-            newView = Instantiate(data.Prefab, modelRoot);
+        // 새로운 캐릭터 View 생성
+        currentView = Instantiate(data.Prefab,modelRoot);
 
-            newView.transform.localPosition = Vector3.zero;
-            newView.transform.localRotation = Quaternion.identity;
-
-            // 생성 직후 캐시에 등록
-            viewCache.Add(data, newView);
-        }
-
-        // 선택된 캐릭터만 활성화
-        newView.SetActive(true);
-
-        currentView = newView;
+        currentView.transform.localPosition = Vector3.zero;
+        currentView.transform.localRotation = Quaternion.identity;
+        currentView.transform.localScale = Vector3.one;
     }
 }
